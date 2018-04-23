0d93c4d997a51ac5189384adf0a3a954c5741617
Steve <Steve.mccann@live.com>
2018-04-23 17:24:05 -0400

    Added base of Irepository and I service

---
+++ src/RoseBloom.Data/RoseBloom.Data/Person/IRelationshipService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RoseBloom.Common.TCL.Entities;
+
+namespace RoseBloom.Data.Common.Person
+{
+    public interface IRelationshipService : IEntityServce
+    {
+       // get we will use base for nor same with list and update and delete 
+    }
+}
