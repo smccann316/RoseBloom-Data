917f3d6c074e165502675b20bc88d2eed58e6df5
Steve <Steve.mccann@live.com>
2018-04-23 16:09:53 -0400

    Added main  RoseBloom.Data classes and inter phases

---
+++ src/RoseBloom.Data/RoseBloom.Data/Results/GetEntityResult.cs
@@ -0,0 +1,9 @@
+﻿namespace RoseBloom.Data.Common
+{
+    public class GetEntityResult<T>
+    {
+        public bool Result { get; set; }
+        public T Entity { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ src/RoseBloom.Data/RoseBloom.Data/Results/UpdateEntityResult.cs
@@ -0,0 +1,10 @@
+﻿namespace RoseBloom.Data.Common
+{
+    public class UpdateEntityResult<T>
+    {
+        public bool Result { get; set; }
+        public T Entity { get; set; }
+        public string Message { get; set; }
+
+    }
+}
