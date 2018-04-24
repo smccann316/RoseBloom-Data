feff8bf7f46b8b37d97eeb48316742e1707cb1f5
Steve <Steve.mccann@live.com>
2018-04-24 10:02:18 -0400

    Deleted the sesult classes and removed services from this library

--- src/RoseBloom.Data/RoseBloom.Data/Results/GetEntityResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoseBloom.Data.Common
-{
-    public class GetEntityResult<T>
-    {
-        public bool Result { get; set; }
-        public T Entity { get; set; }
-        public string Message { get; set; }
-    }
-}
